4153f689a0dc0e4418e9a6ba7c926afe158ecb45
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2019-10-16 15:58:33 +0200

    terraform: add default bucket name valid
    "name" must match [\w+=,.@-]

--- .ci/terraform/s3.tf
+++ .ci/terraform/s3.tf
@@ -14,7 +14,7 @@ module "s3" {
 
   #. bucket_name (required):
   #+ Name of the S3 bucket to create. Only lowercase alphanumeric characters and hyphens allowed.
-  bucket_name              = "<bucket name>"
+  bucket_name              = "bucket-name"
 
   #. bucket_acl (optional): private
   #+ S3 canned ACL: https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl
